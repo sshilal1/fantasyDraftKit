e1045ce903d825a0ba6623a990b6b6b1a431663c
sshilal1 <sshilal1@binghamton.edu>
2016-07-29 13:02:39 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/* ----------Test
+----------------------------------------------------
+------------ Developed by: Stephen Shilale
+*/
+
+var url = "http://www.ourlads.com/nfldepthcharts/depthcharts.aspx";
+var yes;
+
+var teams = [
+	{
+		name: 'Giants',
+		city: 'New York',
+		color: blue
+	},
+	{
+		name: 'Jets',
+		city: 'New York',
+		color: green
+	}
+]
+
+/*
+$.get( url, function( data ) {
+  $( ".result" ).html( data );
+});
+*/
