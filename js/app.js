f78589ca5441646272a1fe3aa66a69cdce3bb215
sshilal1 <sshilal1@binghamton.edu>
2016-08-01 16:02:38 -0400

    started populating teams

--- js/app.js
+++ js/app.js
@@ -4,51 +4,73 @@
 */
 
 var url = "http://www.ourlads.com/nfldepthcharts/depthcharts.aspx";
-var yes;
+var teamID;
 
-var teams = [
-	{
-		id: 'nyg',
-		name: 'Giants',
-		city: 'New York',
-		color1: '#000099',
-		color2: '#0000ff',
-		depth: 'http://www.ourlads.com/nfldepthcharts/depthchart/NYG',
-		offseason: 'http://espn.go.com/nfl/team/transactions/_/name/nyg',
-		logo: 'logos/nyg.jpeg'
+var teams = {
+	// East
+	buf: {
+		name: 'Bills',
+		city: 'Buffalo',
+		color1: '#00338D',
+		color2: '#C60C30'
 	},
-	{
-		id: 'nyj',
+	mia: {
+		name: 'Dolphins',
+		city: 'Miami',
+		color1: '#008E97',
+		color2: '#F58220'
+	},
+	ne: {
+		name: 'Patriots',
+		city: 'New England',
+		color1: '#002244',
+		color2: '#C60C30'
+	},
+	nyj: {
 		name: 'Jets',
 		city: 'New York',
-		color1: '#006600',
-		color2: '#009900',
-		depth: 'http://www.ourlads.com/nfldepthcharts/depthchart/NYG',
-		offseason: 'http://espn.go.com/nfl/team/transactions/_/name/nyg',
-		logo: 'logos/nyg.jpeg'
-	}
-]
+		color1: '#203731',
+		color2: '#203700'
+	},
+	// East
+	nyg: {
+		name: 'Giants',
+		city: 'New York',
+		color1: '#0B2265',
+		color2: '#A71930'
+	},
+};
 
-for (team in teams) {
-	$('#'+teams[team].id)
-	.bind( "mouseenter", function() {
-		console.log(teams[team].color2)
-		$(this).css("background-color", teams[team].color2);
-	});
-}
-/*
-	.mouseout(function() {
-		console.log(teams[team].color1)
-		$(this).css("background-color", teams[team].color1);
-	});*/
-//$('#nyg').mouseover( mouseIn).mouseout( mouseOut);
+$('.team').hover(function() {
+  $(this).css('background-color', teams[this.id].color2);
+}, function() {
+  $(this).css('background-color', teams[this.id].color1);
+});
 
-function mouseIn(color) {
-	$(this).css("background-color", '#000099');
-}
-
-function mouseOut(color) {
-	$(this).css("background-color", '#0000ff');
+for (var t in teams) {
+	// Grab each team
+	var teamBox = document.getElementById(t);
+	
+	// Set colors
+	teamBox.style.background = teams[t].color1;
+	
+	var depthChart = document.createElement('a');
+	var offseasonMoves = document.createElement('a');
+	
+	var depthLoc = "http://www.ourlads.com/nfldepthcharts/depthchart/" + t;
+	var offseasonLoc = "http://espn.go.com/nfl/team/transactions/_/name/" + t;
+	
+	depthChart.innerHTML = "Depth";
+	depthChart.setAttribute("class", "link");
+	depthChart.setAttribute("href", depthLoc);
+	depthChart.setAttribute("target", "_blank");
+	teamBox.appendChild(depthChart);
+	
+	offseasonMoves.innerHTML = "Off Season Moves";
+	offseasonMoves.setAttribute("class", "link");
+	offseasonMoves.setAttribute("href", offseasonLoc);
+	offseasonMoves.setAttribute("target", "_blank");
+	teamBox.appendChild(offseasonMoves);
 }
 
 /*
