689b7cec92310249bd5f5f7b71113322d98e1a34
sshilal1 <sshilal1@binghamton.edu>
2016-08-05 18:13:57 -0400

    Added timer

--- js/draft.js
+++ js/draft.js
@@ -1,9 +1,40 @@
+var draftInfo = {
+	leagueId: 446983,
+	toTeamId: 3,
+	draftToken: "1:446983:3:6033681:1962739774",
+	teams: [{teamId: 1,teamAbbrev: "MCCA",teamLocation: "Team",teamNickname: "McCarthy",owners: [{userProfileId: 38969108,firstName: "Matt",lastName: "McCarthy",userName: "steviejmc1946"}]},{teamId: 2,teamAbbrev: "TM2",teamLocation: "Team",teamNickname: "2",owners: [{userProfileId: 17177134,firstName: "ron ",lastName: "sutton",userName: "ronthepainter1969"}]},{teamId: 3,teamAbbrev: "SHIL",teamLocation: "Team",teamNickname: "Shilale",owners: [{userProfileId: 6033681,firstName: "Stephen",lastName: "Shilale",userName: "Shilaleallstar45"}]},{teamId: 4,teamAbbrev: "ZENO",teamLocation: "Team",teamNickname: "Zenowich",owners: [{userProfileId: 13242723,firstName: "Jason",lastName: "Zenowich",userName: "Jason Zenowich"}]},{teamId: 5,teamAbbrev: "HOUG",teamLocation: "Team",teamNickname: "Hough",owners: [{userProfileId: 13900814,firstName: "Paul",lastName: "Hough",userName: "PaulHough"}]},{teamId: 6,teamAbbrev: "BISH",teamLocation: "Team",teamNickname: "Bishop",owners: [{userProfileId: 356019,firstName: "Andrew",lastName: "Bishop",userName: "Andrew197003"}]},{teamId: 7,teamAbbrev: "TURN",teamLocation: "Team",teamNickname: "Turner",owners: [{userProfileId: 4572489,firstName: "Essex",lastName: "Turner",userName: "HMI_22"}]},{teamId: 8,teamAbbrev: "MOOR",teamLocation: "Team",teamNickname: "Moore",owners: [{userProfileId: 17729938,firstName: "Timothy",lastName: "Moore",userName: "timothy-moore"}]},{teamId: 9,teamAbbrev: "GUTT",teamLocation: "Team",teamNickname: "Gutterman",owners: [{userProfileId: 68030478,firstName: "Michael",lastName: "Gutterman",userName: "mgutterman19"}]},{teamId: 10,teamAbbrev: "ROSA",teamLocation: "Team",teamNickname: "Rosario",owners: [{userProfileId: 16084770,firstName: "Louie",lastName: "Rosario",userName: "nyfinest081"}]},{teamId: 11,teamAbbrev: "BART",teamLocation: "Team",teamNickname: "Barthelemy",owners: [{userProfileId: 18080706,firstName: "Tyler",lastName: "Barthelemy",userName: "STC4ME"}]},{teamId: 12,teamAbbrev: "MALO",teamLocation: "Team",teamNickname: "Malone",owners: [{userProfileId: 4763115,firstName: "Daniel ",lastName: "Malone",userName: "dickiecrickets84"}]}],
+	draftOrder: [1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1],
+	maxRosterSize: 16,
+	positionItems: [{positionId: 0,max: 0,abbrev: "POS0"},{positionId: 1,max: 4,abbrev: "QB"},{positionId: 2,max: 8,abbrev: "RB"},{positionId: 3,max: 8,abbrev: "WR"},{positionId: 4,max: 3,abbrev: "TE"},{positionId: 5,max: 3,abbrev: "K"},{positionId: 6,max: 0,abbrev: "POS6"},{positionId: 7,max: 0,abbrev: "P"},{positionId: 8,max: 0,abbrev: "POS8"},{positionId: 9,max: 0,abbrev: "DT"},{positionId: 10,max: 0,abbrev: "DE"},{positionId: 11,max: 0,abbrev: "LB"},{positionId: 12,max: 0,abbrev: "CB"},{positionId: 13,max: 0,abbrev: "S"},{positionId: 14,max: 0,abbrev: "HC"},{positionId: 15,max: 0,abbrev: "TQB"},{positionId: 16,max: 3,abbrev: "D/ST"}],
+	numIrSlots: 0,
+	proTeams: [{abbrev:"FA",proTeamId:0,tickerAbbrev:"fa"},{abbrev:"Atl",proTeamId:1,tickerAbbrev:"atl"},{abbrev:"Buf",proTeamId:2,tickerAbbrev:"buf"},{abbrev:"Chi",proTeamId:3,tickerAbbrev:"chi"},{abbrev:"Cin",proTeamId:4,tickerAbbrev:"cin"},{abbrev:"Cle",proTeamId:5,tickerAbbrev:"cle"},{abbrev:"Dal",proTeamId:6,tickerAbbrev:"dal"},{abbrev:"Den",proTeamId:7,tickerAbbrev:"den"},{abbrev:"Det",proTeamId:8,tickerAbbrev:"det"},{abbrev:"GB",proTeamId:9,tickerAbbrev:"gnb"},{abbrev:"Ten",proTeamId:10,tickerAbbrev:"ten"},{abbrev:"Ind",proTeamId:11,tickerAbbrev:"ind"},{abbrev:"KC",proTeamId:12,tickerAbbrev:"kan"},{abbrev:"Oak",proTeamId:13,tickerAbbrev:"oak"},{abbrev:"LA",proTeamId:14,tickerAbbrev:"la"},{abbrev:"Mia",proTeamId:15,tickerAbbrev:"mia"},{abbrev:"Min",proTeamId:16,tickerAbbrev:"min"},{abbrev:"NE",proTeamId:17,tickerAbbrev:"nwe"},{abbrev:"NO",proTeamId:18,tickerAbbrev:"nor"},{abbrev:"NYG",proTeamId:19,tickerAbbrev:"nyg"},{abbrev:"NYJ",proTeamId:20,tickerAbbrev:"nyj"},{abbrev:"Phi",proTeamId:21,tickerAbbrev:"phi"},{abbrev:"Ari",proTeamId:22,tickerAbbrev:"ari"},{abbrev:"Pit",proTeamId:23,tickerAbbrev:"pit"},{abbrev:"SD",proTeamId:24,tickerAbbrev:"sdg"},{abbrev:"SF",proTeamId:25,tickerAbbrev:"sfo"},{abbrev:"Sea",proTeamId:26,tickerAbbrev:"sea"},{abbrev:"TB",proTeamId:27,tickerAbbrev:"tam"},{abbrev:"Wsh",proTeamId:28,tickerAbbrev:"was"},{abbrev:"Car",proTeamId:29,tickerAbbrev:"car"},{abbrev:"Jax",proTeamId:30,tickerAbbrev:"jac"},{abbrev:"Bal",proTeamId:33,tickerAbbrev:"bal"},{abbrev:"Hou",proTeamId:34,tickerAbbrev:"hou"}]
+};
+
+for (var t in draftInfo.teams) {
+	var team = 'team' + draftInfo.teams[t].teamId;
+	var teamBox = document.getElementById(team);
+	
+	var teamName = document.createElement('h1');
+	teamName.innerHTML = draftInfo.teams[t].teamAbbrev;
+	teamName.style.float = "left";
+	teamName.style.margin = "10px";
+	teamBox.appendChild(teamName);
+	
+	var owner = document.createElement('h3');
+	owner.innerHTML = draftInfo.teams[t].owners[0].firstName + ' ' + draftInfo.teams[t].owners[0].lastName;
+	owner.style.margin = "10px";
+	teamBox.appendChild(owner);
+
+	console.log(draftInfo.teams[t]);
+}
+
 $("button").click(function(){
 	var token = "1:485416:3:6033681:1291935597";
 	var leagueID = "485416";
 	var url = "http://fantasydraft.espn.com/league-" + leagueID + "/extdraft/json/POLL?&poll=0&token=" + token;
 	url += "&r=" + Math.floor(Math.random()*5000);
 	var newData;
+	drawCanvas(30);
 	
 	$.ajax({url: url, success: function(result){
 		newData = result.replace('draft.processMessage(','');
@@ -14,4 +45,146 @@ $("button").click(function(){
 		console.log(obj);
 	}});
 });
-// /draft.processMessage/g
+
+var counter=0;
+
+function drawCanvas(theCount){
+  var canvas=document.getElementById('mycanvas');
+  var ctx=canvas.getContext('2d');
+  var cWidth=canvas.width;
+  var cHeight=canvas.height;
+  
+  ctx.clearRect(0,0,cWidth,cHeight);
+  
+  var countTo=theCount;
+  
+  var min=Math.floor(countTo/60);
+  var sec=countTo-(min*60);
+  counter = 0;
+  var angle=270;
+  var inc=360/countTo; 
+	
+	setInterval(drawScreen,1000);
+  function drawScreen() {
+
+    //======= reset canvas
+    
+    ctx.fillStyle="#2e3032";
+    ctx.fillRect(0,0,cWidth,cHeight);
+    
+    //========== base arc
+    
+    ctx.beginPath();
+    ctx.strokeStyle="#252424";
+    ctx.lineWidth=14;
+    ctx.arc(cWidth/2,cHeight/2,100,(Math.PI/180)*0,(Math.PI/180)*360,false);
+    ctx.stroke();
+    ctx.closePath();
+    
+    //========== dynamic arc
+    
+    ctx.beginPath();
+    ctx.strokeStyle="#df8209";
+    ctx.lineWidth=14;
+    ctx.arc(cWidth/2,cHeight/2,100,(Math.PI/180)*270,(Math.PI/180)*angle,false);
+    ctx.stroke();
+    ctx.closePath();
+    
+    //======== inner shadow arc
+    
+    grad=ctx.createRadialGradient(cWidth/2,cHeight/2,80,cWidth/2,cHeight/2,115);
+    grad.addColorStop(0.0,'rgba(0,0,0,.4)');
+    grad.addColorStop(0.5,'rgba(0,0,0,0)');
+    grad.addColorStop(1.0,'rgba(0,0,0,0.4)');
+    
+    ctx.beginPath();
+    ctx.strokeStyle=grad;
+    ctx.lineWidth=14;
+    ctx.arc(cWidth/2,cHeight/2,100,(Math.PI/180)*0,(Math.PI/180)*360,false);
+    ctx.stroke();
+    ctx.closePath();
+    
+    //======== bevel arc
+    
+    grad=ctx.createLinearGradient(cWidth/2,0,cWidth/2,cHeight);
+    grad.addColorStop(0.0,'#6c6f72');
+    grad.addColorStop(0.5,'#252424');
+    
+    ctx.beginPath();
+    ctx.strokeStyle=grad;
+    ctx.lineWidth=1;
+    ctx.arc(cWidth/2,cHeight/2,93,(Math.PI/180)*0,(Math.PI/180)*360,true);
+    ctx.stroke();
+    ctx.closePath();
+    
+    //====== emboss arc
+    
+    grad=ctx.createLinearGradient(cWidth/2,0,cWidth/2,cHeight);
+    grad.addColorStop(0.0,'transparent');
+    grad.addColorStop(0.98,'#6c6f72');
+    
+    ctx.beginPath();
+    ctx.strokeStyle=grad;
+    ctx.lineWidth=1;
+    ctx.arc(cWidth/2,cHeight/2,107,(Math.PI/180)*0,(Math.PI/180)*360,true);
+    ctx.stroke();
+    ctx.closePath();
+    
+    //====== Labels
+    
+    var textColor='#646464';
+    var textSize="12";
+    var fontFace="helvetica, arial, sans-serif";
+    
+    ctx.fillStyle=textColor;
+    ctx.font=textSize+"px "+fontFace;
+    ctx.fillText('MIN',cWidth/2-46,cHeight/2-40);
+    ctx.fillText('SEC',cWidth/2+25,cHeight/2-15);
+    
+    //====== Values
+    
+    
+    
+    ctx.fillStyle='#6292ae';
+    
+    if (min>9) {
+      ctx.font='84px '+fontFace;
+      ctx.fillText('9' ,cWidth/2-55,cHeight/2+35);
+      
+      ctx.font='24px '+fontFace;
+      ctx.fillText('+' ,cWidth/2-72,cHeight/2-5);      
+    }
+    else {
+      ctx.font='84px '+fontFace;
+      ctx.fillText(min ,cWidth/2-60,cHeight/2+35);
+    }
+    
+    ctx.font='50px '+fontFace;
+    if (sec<10) {
+      ctx.fillText('0'+sec,cWidth/2+10,cHeight/2+35);
+    } 
+    else {
+      ctx.fillText(sec,cWidth/2+10,cHeight/2+35);
+    }
+    
+    
+    if (sec<=0 && counter<countTo) {
+      angle+=inc;
+      counter++;
+      min--;
+      sec=59; 
+    } else
+      if (counter>=countTo) {
+        sec=0;
+        min=0;
+      } else {
+        angle+=inc;
+        counter++;
+        sec--;
+      }
+  }
+  
+  
+  
+}
+
