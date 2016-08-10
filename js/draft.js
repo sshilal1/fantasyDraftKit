d2adab559fc2d685f93e16e1b1e8dddf874661a5
sshilal1 <sshilal1@binghamton.edu>
2016-08-10 17:20:55 -0400

    Added html player table (need to scrape it) added queryUser to run at beginning ( and ask for draft info )

--- js/draft.js
+++ js/draft.js
@@ -1,27 +1,15 @@
-var draftInfo = {
-	leagueId: 446983,
-	toTeamId: 3,
-	draftToken: "1:446983:3:6033681:1962739774",
-	teams: [{teamId: 1,teamAbbrev: "MCCA",teamLocation: "Team",teamNickname: "McCarthy",owners: [{userProfileId: 38969108,firstName: "Matt",lastName: "McCarthy",userName: "steviejmc1946"}]},{teamId: 2,teamAbbrev: "TM2",teamLocation: "Team",teamNickname: "2",owners: [{userProfileId: 17177134,firstName: "ron ",lastName: "sutton",userName: "ronthepainter1969"}]},{teamId: 3,teamAbbrev: "SHIL",teamLocation: "Team",teamNickname: "Shilale",owners: [{userProfileId: 6033681,firstName: "Stephen",lastName: "Shilale",userName: "Shilaleallstar45"}]},{teamId: 4,teamAbbrev: "ZENO",teamLocation: "Team",teamNickname: "Zenowich",owners: [{userProfileId: 13242723,firstName: "Jason",lastName: "Zenowich",userName: "Jason Zenowich"}]},{teamId: 5,teamAbbrev: "HOUG",teamLocation: "Team",teamNickname: "Hough",owners: [{userProfileId: 13900814,firstName: "Paul",lastName: "Hough",userName: "PaulHough"}]},{teamId: 6,teamAbbrev: "BISH",teamLocation: "Team",teamNickname: "Bishop",owners: [{userProfileId: 356019,firstName: "Andrew",lastName: "Bishop",userName: "Andrew197003"}]},{teamId: 7,teamAbbrev: "TURN",teamLocation: "Team",teamNickname: "Turner",owners: [{userProfileId: 4572489,firstName: "Essex",lastName: "Turner",userName: "HMI_22"}]},{teamId: 8,teamAbbrev: "MOOR",teamLocation: "Team",teamNickname: "Moore",owners: [{userProfileId: 17729938,firstName: "Timothy",lastName: "Moore",userName: "timothy-moore"}]},{teamId: 9,teamAbbrev: "GUTT",teamLocation: "Team",teamNickname: "Gutterman",owners: [{userProfileId: 68030478,firstName: "Michael",lastName: "Gutterman",userName: "mgutterman19"}]},{teamId: 10,teamAbbrev: "ROSA",teamLocation: "Team",teamNickname: "Rosario",owners: [{userProfileId: 16084770,firstName: "Louie",lastName: "Rosario",userName: "nyfinest081"}]},{teamId: 11,teamAbbrev: "BART",teamLocation: "Team",teamNickname: "Barthelemy",owners: [{userProfileId: 18080706,firstName: "Tyler",lastName: "Barthelemy",userName: "STC4ME"}]},{teamId: 12,teamAbbrev: "MALO",teamLocation: "Team",teamNickname: "Malone",owners: [{userProfileId: 4763115,firstName: "Daniel ",lastName: "Malone",userName: "dickiecrickets84"}]}],
-	draftOrder: [1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1],
-	maxRosterSize: 16,
-	positionItems: [{positionId: 0,max: 0,abbrev: "POS0"},{positionId: 1,max: 4,abbrev: "QB"},{positionId: 2,max: 8,abbrev: "RB"},{positionId: 3,max: 8,abbrev: "WR"},{positionId: 4,max: 3,abbrev: "TE"},{positionId: 5,max: 3,abbrev: "K"},{positionId: 6,max: 0,abbrev: "POS6"},{positionId: 7,max: 0,abbrev: "P"},{positionId: 8,max: 0,abbrev: "POS8"},{positionId: 9,max: 0,abbrev: "DT"},{positionId: 10,max: 0,abbrev: "DE"},{positionId: 11,max: 0,abbrev: "LB"},{positionId: 12,max: 0,abbrev: "CB"},{positionId: 13,max: 0,abbrev: "S"},{positionId: 14,max: 0,abbrev: "HC"},{positionId: 15,max: 0,abbrev: "TQB"},{positionId: 16,max: 3,abbrev: "D/ST"}],
-	numIrSlots: 0,
-	proTeams: [{abbrev:"FA",proTeamId:0,tickerAbbrev:"fa"},{abbrev:"Atl",proTeamId:1,tickerAbbrev:"atl"},{abbrev:"Buf",proTeamId:2,tickerAbbrev:"buf"},{abbrev:"Chi",proTeamId:3,tickerAbbrev:"chi"},{abbrev:"Cin",proTeamId:4,tickerAbbrev:"cin"},{abbrev:"Cle",proTeamId:5,tickerAbbrev:"cle"},{abbrev:"Dal",proTeamId:6,tickerAbbrev:"dal"},{abbrev:"Den",proTeamId:7,tickerAbbrev:"den"},{abbrev:"Det",proTeamId:8,tickerAbbrev:"det"},{abbrev:"GB",proTeamId:9,tickerAbbrev:"gnb"},{abbrev:"Ten",proTeamId:10,tickerAbbrev:"ten"},{abbrev:"Ind",proTeamId:11,tickerAbbrev:"ind"},{abbrev:"KC",proTeamId:12,tickerAbbrev:"kan"},{abbrev:"Oak",proTeamId:13,tickerAbbrev:"oak"},{abbrev:"LA",proTeamId:14,tickerAbbrev:"la"},{abbrev:"Mia",proTeamId:15,tickerAbbrev:"mia"},{abbrev:"Min",proTeamId:16,tickerAbbrev:"min"},{abbrev:"NE",proTeamId:17,tickerAbbrev:"nwe"},{abbrev:"NO",proTeamId:18,tickerAbbrev:"nor"},{abbrev:"NYG",proTeamId:19,tickerAbbrev:"nyg"},{abbrev:"NYJ",proTeamId:20,tickerAbbrev:"nyj"},{abbrev:"Phi",proTeamId:21,tickerAbbrev:"phi"},{abbrev:"Ari",proTeamId:22,tickerAbbrev:"ari"},{abbrev:"Pit",proTeamId:23,tickerAbbrev:"pit"},{abbrev:"SD",proTeamId:24,tickerAbbrev:"sdg"},{abbrev:"SF",proTeamId:25,tickerAbbrev:"sfo"},{abbrev:"Sea",proTeamId:26,tickerAbbrev:"sea"},{abbrev:"TB",proTeamId:27,tickerAbbrev:"tam"},{abbrev:"Wsh",proTeamId:28,tickerAbbrev:"was"},{abbrev:"Car",proTeamId:29,tickerAbbrev:"car"},{abbrev:"Jax",proTeamId:30,tickerAbbrev:"jac"},{abbrev:"Bal",proTeamId:33,tickerAbbrev:"bal"},{abbrev:"Hou",proTeamId:34,tickerAbbrev:"hou"}]
-};
-
-for (var t in draftInfo.teams) {
-	var team = 'team' + draftInfo.teams[t].teamId;
+for (i=0;i <12; i++) {
+	var team = 'team' + draftInfo.teams[i].teamId;
 	var teamBox = document.getElementById(team);
 	
 	var teamName = document.createElement('h1');
-	teamName.innerHTML = draftInfo.teams[t].teamAbbrev;
+	teamName.innerHTML = draftInfo.teams[i].teamAbbrev;
 	teamName.style.float = "left";
 	teamName.style.margin = "10px";
 	teamBox.appendChild(teamName);
 	
 	var owner = document.createElement('h3');
-	owner.innerHTML = draftInfo.teams[t].owners[0].firstName + ' ' + draftInfo.teams[t].owners[0].lastName;
+	owner.innerHTML = draftInfo.teams[i].owners[0].firstName + ' ' + draftInfo.teams[i].owners[0].lastName;
 	owner.style.margin = "10px";
 	teamBox.appendChild(owner);
 
@@ -29,13 +17,28 @@ for (var t in draftInfo.teams) {
 }
 
 $("button").click(function(){
-	var token = "1:485416:3:6033681:1291935597";
-	var leagueID = "485416";
-	var url = "http://fantasydraft.espn.com/league-" + leagueID + "/extdraft/json/POLL?&poll=0&token=" + token;
+	var token = draftInfo.draftToken;
+	var leagueID = draftInfo.leagueId;
+	var secondsT = new Date() / 1000;
+	var secondsTi = Math.round(secondsT);
+	console.log(secondsTi);
+	var url = "http://fantasydraft.espn.com/league-" + leagueID + "/extdraft/json/POLL?&poll=" + secondsTi +"&token=" + token;
 	url += "&r=" + Math.floor(Math.random()*5000);
 	var newData;
-	drawCanvas(30);
+	//drawCanvas(30);
+
 	
+	$.ajax({url: url, success: function(result){
+		newData = result.replace('draft.processMessage(','');
+		newData = newData.replace(/\);draft.processMessage.*/,''); 
+		
+		console.log(result);
+		var obj = JSON.parse(newData);
+		
+		console.log(obj);
+	}});
+	
+	/*	
 	$.ajax({
 		type:'GET',
 		url: 'http://games.espn.com/ffl/htmldraft?leagueId=564312&teamId=3&fromTeamId=3',
@@ -46,7 +49,7 @@ $("button").click(function(){
 			console.log(result);
 		}
 	});
-	/*
+
 	$.ajax({
 		url: "http://games.espn.com/ffl/htmldraft?leagueId=551811&teamId=9&fromTeamId=9",
 		dataType: "script",
---
+++ js/lib/queryUser.js
@@ -0,0 +1,39 @@
+var draftInfo = {
+	leagueId: 633199,
+	toTeamId: 4,
+	draftToken: "1:633199:4:6033681:895749697",
+	teams: [{teamId: 1,teamAbbrev: "TM1",teamLocation: "Team",teamNickname: "1",owners: [{userProfileId: 149479065,firstName: "jackson",lastName: "bailey",userName: "baileym@rochester.rr.com"}]},{teamId: 2,teamAbbrev: "GRAG",teamLocation: "Team",teamNickname: "Grages",owners: [{userProfileId: 22460919,firstName: "Steve",lastName: "Grages",userName: "sgrages13"}]},{teamId: 3,teamAbbrev: "WILK",teamLocation: "Team",teamNickname: "Wilkerson ",owners: [{userProfileId: 29105618,firstName: "Mitchell ",lastName: "Wilkerson ",userName: "Mitchell 38"}]},{teamId: 4,teamAbbrev: "SHIL",teamLocation: "Team",teamNickname: "Shilale",owners: [{userProfileId: 6033681,firstName: "Stephen",lastName: "Shilale",userName: "Shilaleallstar45"}]},{teamId: 5,teamAbbrev: "EVER",teamLocation: "Team",teamNickname: "Eversley",owners: [{userProfileId: 28458100,firstName: "Marcus",lastName: "Eversley",userName: "zell88"}]},{teamId: 6,teamAbbrev: "CAST",teamLocation: "Team",teamNickname: "Castillo",owners: [{userProfileId: 128232477,firstName: "Michael",lastName: "Castillo",userName: "Michael13881"}]},{teamId: 7,teamAbbrev: "G",teamLocation: "Team",teamNickname: "G",owners: [{userProfileId: 15760794,firstName: "Michelangelo",lastName: "G",userName: "cubsfanaticmike"}]},{teamId: 8,teamAbbrev: "F.",teamLocation: "Team",teamNickname: "F.",owners: [{userProfileId: 69924169,firstName: "Gabriel",lastName: "Fernandez",userName: "GF0986"}]},{teamId: 9,teamAbbrev: "MOHE",teamLocation: "Team",teamNickname: "moher",owners: [{userProfileId: 16303094,firstName: "adam",lastName: "moher",userName: "amoher1966"}]},{teamId: 10,teamAbbrev: "JONE",teamLocation: "Team",teamNickname: "Jones",owners: [{userProfileId: 137321771,firstName: "Darren",lastName: "Jones",userName: "Muffin_Stuffin40"}]},{teamId: 11,teamAbbrev: "MURR",teamLocation: "Team",teamNickname: "murray",owners: [{userProfileId: 16035870,firstName: "andrew",lastName: "murray",userName: "dru42"}]},{teamId: 12,teamAbbrev: "LOPE",teamLocation: "Team",teamNickname: "Lopez",owners: [{userProfileId: 133271537,firstName: "Tiecal",lastName: "Lopez",userName: "darkzealot1999"}]}],
+	draftOrder: [1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1],
+	maxRosterSize: 16,
+	positionItems: [{positionId: 0,max: 0,abbrev: "POS0"},{positionId: 1,max: 4,abbrev: "QB"},{positionId: 2,max: 8,abbrev: "RB"},{positionId: 3,max: 8,abbrev: "WR"},{positionId: 4,max: 3,abbrev: "TE"},{positionId: 5,max: 3,abbrev: "K"},{positionId: 6,max: 0,abbrev: "POS6"},{positionId: 7,max: 0,abbrev: "P"},{positionId: 8,max: 0,abbrev: "POS8"},{positionId: 9,max: 0,abbrev: "DT"},{positionId: 10,max: 0,abbrev: "DE"},{positionId: 11,max: 0,abbrev: "LB"},{positionId: 12,max: 0,abbrev: "CB"},{positionId: 13,max: 0,abbrev: "S"},{positionId: 14,max: 0,abbrev: "HC"},{positionId: 15,max: 0,abbrev: "TQB"},{positionId: 16,max: 3,abbrev: "D/ST"}],
+	numIrSlots: 0,
+	proTeams: [{abbrev:"FA",proTeamId:0,tickerAbbrev:"fa"},{abbrev:"Atl",proTeamId:1,tickerAbbrev:"atl"},{abbrev:"Buf",proTeamId:2,tickerAbbrev:"buf"},{abbrev:"Chi",proTeamId:3,tickerAbbrev:"chi"},{abbrev:"Cin",proTeamId:4,tickerAbbrev:"cin"},{abbrev:"Cle",proTeamId:5,tickerAbbrev:"cle"},{abbrev:"Dal",proTeamId:6,tickerAbbrev:"dal"},{abbrev:"Den",proTeamId:7,tickerAbbrev:"den"},{abbrev:"Det",proTeamId:8,tickerAbbrev:"det"},{abbrev:"GB",proTeamId:9,tickerAbbrev:"gnb"},{abbrev:"Ten",proTeamId:10,tickerAbbrev:"ten"},{abbrev:"Ind",proTeamId:11,tickerAbbrev:"ind"},{abbrev:"KC",proTeamId:12,tickerAbbrev:"kan"},{abbrev:"Oak",proTeamId:13,tickerAbbrev:"oak"},{abbrev:"LA",proTeamId:14,tickerAbbrev:"la"},{abbrev:"Mia",proTeamId:15,tickerAbbrev:"mia"},{abbrev:"Min",proTeamId:16,tickerAbbrev:"min"},{abbrev:"NE",proTeamId:17,tickerAbbrev:"nwe"},{abbrev:"NO",proTeamId:18,tickerAbbrev:"nor"},{abbrev:"NYG",proTeamId:19,tickerAbbrev:"nyg"},{abbrev:"NYJ",proTeamId:20,tickerAbbrev:"nyj"},{abbrev:"Phi",proTeamId:21,tickerAbbrev:"phi"},{abbrev:"Ari",proTeamId:22,tickerAbbrev:"ari"},{abbrev:"Pit",proTeamId:23,tickerAbbrev:"pit"},{abbrev:"SD",proTeamId:24,tickerAbbrev:"sdg"},{abbrev:"SF",proTeamId:25,tickerAbbrev:"sfo"},{abbrev:"Sea",proTeamId:26,tickerAbbrev:"sea"},{abbrev:"TB",proTeamId:27,tickerAbbrev:"tam"},{abbrev:"Wsh",proTeamId:28,tickerAbbrev:"was"},{abbrev:"Car",proTeamId:29,tickerAbbrev:"car"},{abbrev:"Jax",proTeamId:30,tickerAbbrev:"jac"},{abbrev:"Bal",proTeamId:33,tickerAbbrev:"bal"},{abbrev:"Hou",proTeamId:34,tickerAbbrev:"hou"}]
+};
+
+com = { espn: {
+	env: {
+		av: navigator.appVersion,
+		ua: navigator.userAgent,
+		an: navigator.appName,
+		platform: navigator.platform,
+		IE: "Microsoft Internet Explorer",
+		NS: "Netscape",
+		MAC: "MacPPC",
+
+		host: "games.espn.com",
+		context: "clubhouse"
+	},
+	listeners: {},
+	games: {
+		gameRoot: "ffl",
+		leagueId: draftInfo.leagueId,
+		seasonId: 2016,
+		toTeamId: draftInfo.toTeamId,
+		fromTeamId: draftInfo.toTeamId,
+		scoringPeriodId: 1,
+		currentScoringPeriodId: 1,
+		leagueType: 0
+	}
+}};
+com.espn.games.isLeagueManager = false;com.espn.games.isLeagueCreator = false;com.espn.games.isLeagueMember = true;com.espn.games.userLoggedIn = false;com.espn.games.username = draftInfo.teams[draftInfo.toTeamId].owners[0].userName;
+var GAMEROOT = "ffl";var host = location.hostname;var gameRoot = "ffl";var leagueId = draftInfo.leagueId;var teamId = draftInfo.toTeamId;
