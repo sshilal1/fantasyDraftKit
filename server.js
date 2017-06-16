b45fcb39b9f430d1bb2b5eb652c34fa3e4b52792
Stephen Shilale <sshilal1@binghamton.edu>
2017-06-16 10:29:31 -0400

    Added gitignore, found player id by searching through team

--- server.js
+++ server.js
@@ -41,6 +41,239 @@ app.get('/scrape', function(req, res){
     })
 })
 
+var teams = {
+	// East
+	buf: {
+		name: 'Bills',
+		city: 'Buffalo',
+		color1: '#00338D',
+		color2: '#C60C30'
+	},
+	mia: {
+		name: 'Dolphins',
+		city: 'Miami',
+		color1: '#008E97',
+		color2: '#F58220'
+	},
+	ne: {
+		name: 'Patriots',
+		city: 'New England',
+		color1: '#002244',
+		color2: '#C60C30'
+	},
+	nyj: {
+		name: 'Jets',
+		city: 'New York',
+		color1: '#203700',
+		color2: '#ffffff'
+	},
+	// North
+	bal: {
+		name: 'Ravens',
+		city: 'Baltimore',
+		color1: '#241773',
+		color2: '#9E7C0C'
+	},
+	cin: {
+		name: 'Bengals',
+		city: 'Cincinatti',
+		color1: '#FB4F14',
+		color2: '#000000'
+	},
+	cle: {
+		name: 'Browns',
+		city: 'Cleveland',
+		color1: '#FB4F14',
+		color2: '#22150C'
+	},
+	pit: {
+		name: 'Steelers',
+		city: 'Pittsburgh',
+		color1: '#FFB612',
+		color2: '#000000'
+	},
+	// South
+	hou: {
+		name: 'Texans',
+		city: 'Houston',
+		color1: '#03202F',
+		color2: '#A71930'
+	},
+	ind: {
+		name: 'Colts',
+		city: 'Indianapolis',
+		color1: '#002C5F',
+		color2: '#A5ACAF'
+	},
+	jac: {
+		name: 'Jaguars',
+		city: 'Jacksonville',
+		color1: '#006778',
+		color2: '#D7A22A'
+	},
+	ten: {
+		name: 'Titans',
+		city: 'Tennessee',
+		color1: '#002244',
+		color2: '#4B92DB'
+	},
+	// West
+	den: {
+		name: 'Broncos',
+		city: 'Denver',
+		color1: '#002244',
+		color2: '#FB4F14'
+	},
+	kc: {
+		name: 'Chiefs',
+		city: 'Kansas City',
+		color1: '#E31837',
+		color2: '#000000'
+	},
+	oak: {
+		name: 'Raiders',
+		city: 'Oakland',
+		color1: '#A5ACAF',
+		color2: '#000000'
+	},
+	sd: {
+		name: 'Chargers',
+		city: 'San Diego',
+		color1: '#FFB612',
+		color2: '#002244'
+	},
+	// NFC
+	// East
+	dal: {
+		name: 'Cowboys',
+		city: 'Dallas',
+		color1: '#002244',
+		color2: '#B0B7BC'
+	},
+	nyg: {
+		name: 'Giants',
+		city: 'New York',
+		color1: '#0B2265',
+		color2: '#A71930'
+	},
+	phi: {
+		name: 'Eagles',
+		city: 'Philidelphia',
+		color1: '#004953',
+		color2: '#A5ACAF'
+	},
+	was: {
+		name: 'Redskins',
+		city: 'Washington',
+		color1: '#773141',
+		color2: '#FFB612'
+	},
+	// North
+	chi: {
+		name: 'Bears',
+		city: 'Chicago',
+		color1: '#0B162A',
+		color2: '#C83803'
+	},
+	det: {
+		name: 'Lions',
+		city: 'Detroit',
+		color1: '#005A8B',
+		color2: '#B0B7BC'
+	},
+	gb: {
+		name: 'Packers',
+		city: 'Green Bay',
+		color1: '#203731',
+		color2: '#FFB612'
+	},
+	min: {
+		name: 'Vikings',
+		city: 'Minnesota',
+		color1: '#4F2683',
+		color2: '#FFC62F'
+	},
+	// South
+	atl: {
+		name: 'Falcons',
+		city: 'Atlanta',
+		color1: '#A71930',
+		color2: '#000000'
+	},
+	car: {
+		name: 'Panthers',
+		city: 'Carolina',
+		color1: '#0085CA',
+		color2: '#000000'
+	},
+	no: {
+		name: 'Saints',
+		city: 'New Orleans',
+		color1: '#9F8958',
+		color2: '#000000'
+	},
+	tb: {
+		name: 'Buccaneers',
+		city: 'Tampa Bay',
+		color1: '#D50A0A',
+		color2: '#000000'
+	},
+	// West
+	arz: {
+		name: 'Cardinals',
+		city: 'Arizona',
+		color1: '#97233F',
+		color2: '#000000'
+	},
+	ram: {
+		name: 'Rams',
+		city: 'Los Angeles',
+		color1: '#002244',
+		color2: '#B3995D'
+	},
+	sf: {
+		name: '49ers',
+		city: 'San Francisco',
+		color1: '#AA0000',
+		color2: '#B3995D'
+	},
+	sea: {
+		name: 'Seahawks',
+		city: 'Seattle',
+		color1: '#002244',
+		color2: '#69BE28'
+	}
+};
+
+var x = '.tablehead';
+
+//for (team in teams) {
+	team = 'nyg';
+	url = 'http://www.espn.com/nfl/team/roster/_/name/' + team;
+	
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+			
+			var name, position, heightweight, team, links, id;
+			var json = { name : "", position : "", heightweight : "", team : ""};
+
+			$('.tablehead').filter(function(){
+				var data = $(this);
+				
+				firstPlayer =  data.children().children().eq(2);
+				links = firstPlayer.children().eq(1).html().toString();
+				id = links.match(/\d+/)[0];
+				name = firstPlayer.children().eq(1).text();
+				
+			})
+						
+			console.log(name);
+			console.log(id);
+		}
+	})
+//}
+
 ids = [14879,2977644]
 url = 'http://www.espn.com/nfl/player/stats/_/id/2977644/';
 
