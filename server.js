ae35aaae2c80d424038b63c4d1b9011aaca98f95
Stephen Shilale <sshilal1@binghamton.edu>
2017-06-16 14:19:29 -0400

    added mssql for amazon rds

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+const sql   = require('mssql');
 
 app.get('/scrape', function(req, res){
 
