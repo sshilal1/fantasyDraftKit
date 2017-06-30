9014321e05d3ddee0df251d974ad4d0fd4a6a49e
Stephen Shilale <sshilal1@binghamton.edu>
2017-06-30 16:23:18 -0400

    got sql connection working by allowing port 3306 for ALL IPs(need to figure out why

--- mysql.js
+++ mysql.js
@@ -1,18 +1,22 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "fantasydb"
+	host: "***",
+	user: "sshilal1",
+	password: "***",
+	database: "fantasykit",
+	port: 3306
 });
 
 con.connect(function(err) {
 	if (err) throw err;
 	console.log("Connected!");
+	
 	var sql = "CREATE TABLE teams (id INTEGER PRIMARY KEY, city VARCHAR(255), name VARCHAR(255), abbrev)";
 	con.query(sql, function (err, result) {
 		if (err) throw err;
 		console.log("Table created");
 	});
 });
+
+con.end();
