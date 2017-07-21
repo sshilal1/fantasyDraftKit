e3cfa3cd25a3a4020248ccd6da03493bb8d285dc
Stephen Shilale <sshilal1@binghamton.edu>
2017-07-21 15:50:02 -0400

    fixed indenting

--- fantasykit-webpack-react/src/js/components/Card.js
+++ fantasykit-webpack-react/src/js/components/Card.js
@@ -11,43 +11,43 @@ export default class PlayerCard extends React.Component {
     return (
       <div className="Card">
         <Flexbox className="card-row header" flexDirection="row" justifyContent="space-between">
+			<div>
+				<div className="text">Mathias</div>
+				<div className="xxlarge text">KIWANUKA</div>
+			</div>
+			<div className="xlarge text">QB</div>
+        </Flexbox>
+		<Flexbox className="card-row" flexDirection="row" justifyContent="space-between">
+			<img className="player-img" src={"http://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5526.png&amp;w=345&amp;h=230;"}/>
+			<Flexbox className="number-team" flexDirection="column" justifyContent="center">
+				<div className="center  xlarge text">NYG</div>
+				<div className="center text">#10</div>
+			</Flexbox>
+        </Flexbox>
+		<div className="card-row">
+			<div className="rankings-title">
+				<Flexbox flexDirection="row" justifyContent="space-between">
+					<FlatButton style={{minWidth:"40px"}} className="text" /*onClick={() => this.setState({ sortKey: 'TOTAL' })}*/ >Total</FlatButton>
+					<FlatButton style={{minWidth:"45px"}}><img style={{width:"80%"}} src={espn}/></FlatButton>
+					<FlatButton style={{minWidth:"85px"}}><img src={pros}/></FlatButton>
+				</Flexbox>
+				<Flexbox flexDirection="row" justifyContent="space-around">
 					<div>
-						<div className="text">Mathias</div>
-						<div className="xxlarge text">KIWANUKA</div>
+						<div className="center text">Overall</div>
+						<div className="center xlarge text">29</div>
 					</div>
-					<div className="xlarge text">QB</div>
-        </Flexbox>
-				<Flexbox className="card-row" flexDirection="row" justifyContent="space-between">
-          <img className="player-img" src={"http://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5526.png&amp;w=345&amp;h=230;"}/>
-					<Flexbox className="number-team" flexDirection="column" justifyContent="center">
-						<div className="center  xlarge text">NYG</div>
-						<div className="center text">#10</div>
-					</Flexbox>
-        </Flexbox>
-				<div className="card-row">
-          <div className="rankings-title">
-						<Flexbox flexDirection="row" justifyContent="space-between">
-							<FlatButton style={{minWidth:"40px"}} className="text" /*onClick={() => this.setState({ sortKey: 'TOTAL' })}*/ >Total</FlatButton>
-							<FlatButton style={{minWidth:"45px"}}><img style={{width:"80%"}} src={espn}/></FlatButton>
-							<FlatButton style={{minWidth:"85px"}}><img src={pros}/></FlatButton>
-						</Flexbox>
-						<Flexbox flexDirection="row" justifyContent="space-around">
-							<div>
-								<div className="center text">Overall</div>
-								<div className="center xlarge text">29</div>
-							</div>
-							<div>
-								<div className="center text">Positional</div>
-								<div className="center xlarge text">6</div>
-							</div>
-						</Flexbox>
+					<div>
+						<div className="center text">Positional</div>
+						<div className="center xlarge text">6</div>
 					</div>
+				</Flexbox>
+			</div>
         </div>
-				<div className="card-row">
-          <FlatButton className="text large" label="Stats"/>
-					<FlatButton className="text large" label="Bio"/>
+		<div className="card-row">
+			<FlatButton className="text large" label="Stats"/>
+			<FlatButton className="text large" label="Bio"/>
         </div>
-      </div>
+    </div>
     );
   }
 }
