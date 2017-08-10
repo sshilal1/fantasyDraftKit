2ca5ca34ad1f700535cc91cc1ebeb9b0c8f2f715
Stephen Shilale <sshilal1@binghamton.edu>
2017-08-10 18:38:00 -0400

    implemented basic router

--- src/js/client.js
+++ src/js/client.js
@@ -1,8 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Switch from "./components/PageSwitch";
+//import { Router, Route, IndexRoute } from "react-router";
+import {
+  HashRouter,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import Players from "./pages/Players";
+import Comparisons from "./pages/Comparisons";
 
-const app = document.getElementById('app');
+//const app = document.getElementById('app');
+//ReactDOM.render(<Players/>, app);
 
-ReactDOM.render(<Players/>, app);
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Players}/>
+      <Route path='/comparisons' component={Comparisons}/>
+    </Switch>
+  </main>
+)
+
+// The Header creates links that can be used to navigate
+// between routes.
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/comparisons'>Comparisons</Link></li>
+      </ul>
+    </nav>
+  </header>
+)
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
+
+// This demo uses a HashRouter instead of BrowserRouter
+// because there is no server to match URLs
+ReactDOM.render((
+  <HashRouter>
+    <App />
+  </HashRouter>
+), document.getElementById('app'))
---
+++ src/js/pages/Comparisons.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default class Comparisons extends React.Component {
+  constructor() {
+    super();
+  }
+  /*
+  componentWillMount() {
+    TodoStore.on("change", this.getTodos);
+  }
+
+  componentWillUnmount() {
+    TodoStore.removeListener("change", this.getTodos);
+  }
+
+  getTodos() {
+    this.setState({
+      todos: TodoStore.getAll(),
+    });
+  }
+
+  reloadTodos() {
+    TodoActions.reloadTodos();
+  }
+
+  render() {
+    const { todos } = this.state;
+
+    const TodoComponents = todos.map((todo) => {
+        return <Todo key={todo.id} {...todo}/>;
+    });
+    return (
+      <div>
+        <button onClick={this.reloadTodos.bind(this)}>Reload!</button>
+        <h1>Todos</h1>
+        <ul>{TodoComponents}</ul>
+      </div>
+    );
+  }*/
+  render() {
+    return (
+      <div> Comparison Page! </div>
+    )
+  }
+}
