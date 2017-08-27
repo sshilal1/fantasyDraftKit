a32c36669dc3410c3b9905542e55f0c7c9bc4e72
Stephen Shilale <sshilal1@binghamton.edu>
2017-08-27 13:58:07 -0400

    integrated rushing stats block

--- src/js/components/StatLines/Comparisons/RushingStatsBlock.js
+++ src/js/components/StatLines/Comparisons/RushingStatsBlock.js
@@ -1,17 +1,53 @@
 import React from "react";
 import Flexbox from 'flexbox-react';
 
+import ComparisonStore from "../../../stores/ComparisonStore";
+
+const noseason = {
+  att:"-",
+  yds:"-",
+  avg:"-",
+  td:"-",
+  fl: "-"
+};
+
 export default class RushingStatsBlock extends React.Component {
 
   constructor(props) {
     super(props);
-    console.log(props.stats);
-    this.state = {
-      att:442,
-      yds:1203,
-      avg:5.3,
-      td:11,
-      fl:4
+    this.updateYear = this.updateYear.bind(this);
+    this.state = noseason;
+  }
+
+  componentWillMount() {
+    ComparisonStore.on("yearchange", this.updateYear);
+  }
+
+  componentWillUnmount() {
+    ComparisonStore.removeListener("yearchange", this.updateYear);
+  }
+
+  componentDidMount() {
+    this.updateYear(this.props.stats[0].yr);
+  }
+
+  updateYear(year) {
+
+    const seasons = this.props.stats;
+
+    var index = _.findIndex(seasons, function(o) { return o.yr.toString() == year; });
+
+    if (index != -1) {
+      this.setState({
+        att:seasons[index].att,
+        yds:seasons[index].yds,
+        avg:seasons[index].avg,
+        td:seasons[index].td,
+        fl:seasons[index].fl,
+      });
+    }
+    else {
+      this.setState(noseason);
     }
   }
 
